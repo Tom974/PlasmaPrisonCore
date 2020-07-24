35e4e4543087fe01a15c934c878ff63bb9dab37f
Fede <fendt873@gmail.com>
2020-07-24 16:41:46 +0200

    Fixed some bugs + EnchantManager done TODO: Fix Glow.java

--- src/main/java/me/fede1132/plasmaprisoncore/events/CommandPreProcess.java
+++ src/main/java/me/fede1132/plasmaprisoncore/events/CommandPreProcess.java
@@ -1,12 +1,14 @@
 package me.fede1132.plasmaprisoncore.events;
 
 import me.fede1132.plasmaprisoncore.PlasmaPrisonCore;
+import me.fede1132.plasmaprisoncore.addons.enchant.Enchant;
 import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class CommandPreProcess implements Listener {
     private final PlasmaPrisonCore instance = PlasmaPrisonCore.getInstance();
@@ -56,6 +58,30 @@ public class CommandPreProcess implements Listener {
                     return;
                 }
                 event.getPlayer().sendMessage(ChatColor.RED + "No addon found for provided input "
+                        + ChatColor.WHITE + args[2]);
+                return;
+            }
+            case "enchant": {
+                if (args.length<4||event.getPlayer().getInventory().getItemInMainHand()==null) return;
+                if (!instance.enchantManager.registeredEnchants.containsKey(args[2])) {
+                    event.getPlayer().sendMessage(args[2] + ChatColor.RED + " is not a valid enchantment!");
+                    return;
+                }
+                int i;
+                try {
+                    i=Integer.parseInt(args[3]);
+                } catch (NumberFormatException e) {
+                    event.getPlayer().sendMessage(args[3] + ChatColor.RED + " is not a valid number!");
+                    return;
+                }
+                Enchant enchant = instance.enchantManager.registeredEnchants.get(args[2]);
+                if (i>enchant.max) {
+                    event.getPlayer().sendMessage(ChatColor.RED + "The maximum enchant level for this enchantment is " + ChatColor.WHITE + enchant.max);
+                    return;
+                }
+                ItemStack hand = event.getPlayer().getInventory().getItemInMainHand();
+                event.getPlayer().getInventory().setItemInMainHand(instance.enchantManager.enchant(hand,enchant,i));
+                event.getPlayer().sendMessage(ChatColor.GREEN + "Successfully enchant the held item with "
                         + ChatColor.WHITE + args[2]);
                 return;
             }
