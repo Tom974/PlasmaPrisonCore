acfbfb020f4a4863d0fd79c603d869bba46d1548
Tom974 <55812394+Tom974@users.noreply.github.com>
2023-02-13 15:49:28 +0100

    fix(playerleave): also remove key from hashmap on  leave xd

--- src/main/java/me/fede1132/plasmaprisoncore/events/PlayerLeave.java
+++ src/main/java/me/fede1132/plasmaprisoncore/events/PlayerLeave.java
@@ -4,18 +4,13 @@ import me.fede1132.plasmaprisoncore.PlasmaPrisonCore;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 public class PlayerLeave implements Listener {
     private final PlasmaPrisonCore plugin = PlasmaPrisonCore.getInstance();
     @EventHandler(priority = EventPriority.LOW)
     public void onJoin(PlayerQuitEvent event) {
         plugin.database.onLeave(event.getPlayer().getName(), event.getPlayer().getUniqueId());
+        plugin.tokens.remove(event.getPlayer().getUniqueId()); // remove from hashmap as its not needed anymore. aka fix memory
     }
 }
