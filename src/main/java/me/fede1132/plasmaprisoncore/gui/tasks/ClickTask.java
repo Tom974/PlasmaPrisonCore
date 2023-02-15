c5c0973fad85dfe14e144db631a9ecc625636a99
Tom974 <55812394+Tom974@users.noreply.github.com>
2023-02-15 13:54:48 +0100

    delete(gui): delete gui folder

--- src/main/java/me/fede1132/plasmaprisoncore/gui/tasks/ClickTask.java
+++
@@ -1,8 +0,0 @@
-package me.fede1132.plasmaprisoncore.gui.tasks;
-
-import me.fede1132.plasmaprisoncore.gui.Icon;
-import org.bukkit.event.inventory.InventoryClickEvent;
-
-public interface ClickTask {
-  void fire(Icon paramIcon, InventoryClickEvent paramInventoryClickEvent);
-}
